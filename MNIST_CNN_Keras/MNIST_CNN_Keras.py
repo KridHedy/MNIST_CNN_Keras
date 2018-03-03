dcc199fbe8898d73c0006db0c7b4a8cdb6c2c03c
KridHedy <34186647+KridHedy@users.noreply.github.com>
2018-03-03 16:27:49 +0100

    Last Commit

--- MNIST_CNN_Keras/MNIST_CNN_Keras.py
+++ MNIST_CNN_Keras/MNIST_CNN_Keras.py
@@ -2,12 +2,13 @@
 from keras.datasets import mnist
 from matplotlib import pyplot as plt
 from keras import utils as np_utils
-from keras.models import Sequential
+from keras.models import Sequential,model_from_json
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 
 import numpy as np
 np.random.seed(123)  # for reproducibility
+
 
 
 def preprocessing_data():
@@ -45,10 +46,10 @@ def preprocessing_labels():
     print (y_train.shape)  #60000 train data  , now we have 10 classes
     print (y_test.shape)   #10000 test data
 
-
+    
 def Model_creation_evaluation():
     ####### Creating  the model architecture #######
-
+    global model
     model = Sequential()
     #inuput layer
     model.add(Convolution2D(32, (3,3), activation='relu', input_shape=(1,28,28),data_format='channels_first'))      # 32 : Filters :the number of output filters in the convolution.
@@ -73,18 +74,40 @@ def Model_creation_evaluation():
 
     #Fitting the data into the model, precising the batch size and epochs num
 
+def Training_data():
     model.fit(x_train,y_train,batch_size=32, epochs=10, verbose=1)
-    return model
+
     #testing the model on test data 
 
     classes = model.predict(x_test, batch_size=128)
-
+    
     score = model.evaluate(x_train,y_train,verbose=0)
 
+    #print("\n%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
 
-    print("\n%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+def save_model():
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("--- Saved model =) ---")
 
+def load_model():
+    global loaded_model
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("--- Loaded model from disk =) ---")
 
+def test_loaded_model():
+    loaded_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    score = loaded_model.evaluate(x_train,y_train,verbose=0)
+    print("\n%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
 
 if __name__ == '__main__':
     #load data 
@@ -92,8 +115,11 @@ if __name__ == '__main__':
     preprocessing_data()
     preprocessing_labels()
     Model_creation_evaluation()
+    #Training_data()
+    #save_model()
+    load_model()
+    test_loaded_model()
    
-
 
 
 
